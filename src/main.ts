f4cf89ec8d569480057cd98a213033999ca35dad
Shubham Vast <shubhamvast5@gmail.com>
2024-08-28 10:45:11 +0530

    itercept incoming response

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn){
   console.log("Outgoing request");
@@ -13,7 +14,18 @@ function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn){
   // console.log(request);  
   // return next(reqModify);
 
-  return next(request);
+  return next(request)
+  .pipe(
+    tap({
+      next: event =>{
+        if(event.type === HttpEventType.Response){
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);                    
+        }
+      }
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, { 
